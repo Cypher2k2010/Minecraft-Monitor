3a6fe8d6c0ae89bf7a5a66306a960895289770d2
Stefan Kent <stefan@icebergcms.com.au>
2012-08-09 14:36:31 +1000

    Created stand alone branch

--- config.inc.php
+++ config.inc.php
@@ -4,26 +4,26 @@
 	Mincraft Monitor Webservice object: Version 0.2
 	http://www.twosphere.com.au
 	http://www.minecraftmonitor.com.au
-	
+
 	All code is provided by Twosphere for testing purposes only.
 	This code has not been thoroughly tested under all conditions. Twosphere, therefore, cannot guarantee or imply reliability, serviceability, or function of these programs.
 	All programs contained herein are provided to you "AS IS" without any warranties of any kind.
 	The implied warranties of non-infringement, merchantability and fitness for a particular purpose are expressly disclaimed.
 
-	date: 2011-07-23	
+	date: 2011-07-23
 	File: config.inc.php
 	Global configuration for webservices.
-	*/	
+	*/
 
 	//Security, ignore this line
 	if (
-	!empty($_SERVER['SCRIPT_FILENAME']) && 'comm.php' != basename($_SERVER['SCRIPT_FILENAME']) 
-										&& 'player.php' != basename($_SERVER['SCRIPT_FILENAME']) 
-										&& 'index.php' != basename($_SERVER['SCRIPT_FILENAME'])	
+	!empty($_SERVER['SCRIPT_FILENAME']) && 'comm.php' != basename($_SERVER['SCRIPT_FILENAME'])
+										&& 'player.php' != basename($_SERVER['SCRIPT_FILENAME'])
+										&& 'index.php' != basename($_SERVER['SCRIPT_FILENAME'])
 	) die ('Loading this page directly will bring dishonor to your family.');
 
-	//Config String Start	
-	$json_settings = '{"MINECRAFT_PATH":".\/minecraft","SERVER_PASS":"","LOG_LINES":40,"CENTRAL_UPDATE":false,"UPDATE_SELF":false,"UPDATE_MINECRAFT":true,"MINECRAFT_BIN":"minecraft-server.jar","TEAMSPEAK_BIN":"\/proc\/net\/dev","NETDEV_PATH":"\/proc\/net\/dev","CPU_PATH":"\/proc\/cpuinfo","LOAD_PATH":"\/proc\/loadavg","MONITOR_HANDLE":"","JAVA_PATH":"java\/"}';
+	//Config String Start
+	$json_settings = '{"MINECRAFT_PATH":".\/minecraft","SERVER_PASS":"","LOG_LINES":40,"CENTRAL_UPDATE":false,"UPDATE_SELF":false,"UPDATE_MINECRAFT":true,"MINECRAFT_BIN":"minecraft-server.jar","NETDEV_PATH":"\/proc\/net\/dev","CPU_PATH":"\/proc\/cpuinfo","LOAD_PATH":"\/proc\/loadavg","MONITOR_HANDLE":"","JAVA_PATH":"java\/"}';
 	//Config String End
 
 	$json_array = json_decode($json_settings,true);
@@ -39,12 +39,12 @@
 				if($value != "")
 					define($key,$_SERVER["DOCUMENT_ROOT"]."/".$value);
 			break;
-		
+
 			default:
 				define($key,$value);
 			break;
 		}
-		
+
 	}
 
 ?>
--- index.php
+++ index.php
@@ -4,17 +4,19 @@
 	Mincraft Monitor Webservice object: Version 0.2
 	http://www.twosphere.com.au
 	http://www.minecraftmonitor.com.au
-	
+
 	All code is provided by Twosphere for testing purposes only.
 	This code has not been thoroughly tested under all conditions. Twosphere, therefore, cannot guarantee or imply reliability, serviceability, or function of these programs.
 	All programs contained herein are provided to you "AS IS" without any warranties of any kind.
 	The implied warranties of non-infringement, merchantability and fitness for a particular purpose are expressly disclaimed.
 
-	date: 2011-07-23	
+	date: 2011-07-23
 	File: index.php
 	Viewer redirection.
-	*/	
-	
+	*/
+
 	ob_start();include("config.inc.php");ob_end_clean();
-	header("Location: http://www.minecraftmonitor.com/view-".MONITOR_HANDLE);
+
+	echo "test output";
+
 ?>
